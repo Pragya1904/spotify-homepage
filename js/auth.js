604b569dd72ae5920f99159e6262f31962302809
Sumit kumar <sumitkumar@Sumits-MacBook-Air.local>
2022-02-19 18:50:51 +0530

    support page enhanced

--- js/auth.js
+++ js/auth.js
@@ -7,7 +7,9 @@ const getUser = () => {
       },
     })
     .then((r) => {
+      console.log(r.status);
       const { data } = r;
+      console.log(data);
       const non_auth_links = document.querySelectorAll(".non_auth");
       Array.from(non_auth_links).forEach((link) => {
         link.style.display = "none";
@@ -16,8 +18,21 @@ const getUser = () => {
         ".user-avatar"
       ).innerHTML += `<img src="${data.images[0].url}" alt="${data.display_name}">`;
     })
-    .catch((err) => {
-      console.log(err);
+    .catch(async (err) => {
+      if (err.response && err.response.data) {
+        const { error } = err.response.data;
+        if (error) {
+          try {
+            const r = await axios.post("http://localhost:5000/refresh_token", {
+              refresh_token: Cookies.get("refresh_token"),
+            });
+            Cookies.set("access_token", r.data.access_token);
+            window.location.reload();
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      }
     });
 };
 
--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
-const menu_btn = document.querySelector('.menu-btn');
-const mobile_menu = document.querySelector('#mobile-nav');
-const mobile_menu_close = document.querySelector('.mobile-menu-close');
-menu_btn.addEventListener('click', function() {
-    console.log('clicked');
-    mobile_menu.classList.add('show');
-})
+const menu_btn = document.querySelector(".menu-btn");
+const mobile_menu = document.querySelector("#mobile-nav");
+const mobile_menu_close = document.querySelector(".mobile-menu-close");
+menu_btn.addEventListener("click", function () {
+  console.log("clicked");
+  mobile_menu.classList.add("show");
+});
 
-mobile_menu_close.addEventListener('click', function() {
-    mobile_menu.classList.remove('show');
-})
+mobile_menu_close.addEventListener("click", function () {
+  mobile_menu.classList.remove("show");
+});
 
-
-
-
+const authenticated_link = document.querySelector(".authenticated");
+authenticated_link.addEventListener("click", function () {
+  document
+    .querySelector(".profile-menubar")
+    .classList.toggle("dropdown-enable");
+});
